1974a62b304422d3a24c277068214c07d31603d6
James Preiss <japreiss@usc.edu>
2018-02-02 22:18:31 -0800

    endless tweaks to cartpole_batch

--- gym/envs/classic_sysid/cartpole_batch.py
+++ gym/envs/classic_sysid/cartpole_batch.py
@@ -23,21 +23,21 @@ class CartPoleEnvSysIDBatch(gym.Env):
     def __init__(self):
         self.N = 32
         self.state = np.zeros((self.N, 8))
-        self.fixedlen = False
+        self.fixedlen = True
 
         # initialize the system identification parameters.
         def expand(x):
             ratio = 4.0
             return [x/ratio, x*ratio]
         self.sysid_params = OrderedDict([
-            ('masscart',  [expand(1.0), 0]),
-            ('masspole',  [expand(0.1), 0]),
-            ('length',    [expand(0.5), 0]),
-            ('force_mag', [expand(10.0), 0]),
-            #('masscart',  [[1, 1], 0]),
-            #('masspole',  [[0.1, 0.1], 0]),
-            #('length',    [[0.5, 0.5], 0]),
-            #('force_mag', [[20, 20], 0]),
+            #('masscart',  [expand(1.0), 0]),
+            #('masspole',  [expand(0.1), 0]),
+            #('length',    [expand(0.5), 0]),
+            #('force_mag', [expand(10.0), 0]),
+            ('masscart',  [[1, 1], 0]),
+            ('masspole',  [[0.1, 0.1], 0]),
+            ('length',    [[0.5, 0.5], 0]),
+            ('force_mag', [[10, 10], 0]),
         ])
         # self.masscart = 1.0
         # self.masspole = 0.1
@@ -73,7 +73,7 @@ class CartPoleEnvSysIDBatch(gym.Env):
         self.viewer = None
         self.steps_beyond_done = None
 
-        self.ep_len = 20
+        self.ep_len = 512
         self.tick = 0
 
         self._seed()
@@ -116,33 +116,44 @@ class CartPoleEnvSysIDBatch(gym.Env):
         temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
         thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
         xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        x  = x + self.tau * x_dot
-        x_dot = x_dot + self.tau * xacc
-        theta = theta + self.tau * theta_dot
         damping = 0.998 # lose 10% of velocity in one second
+        x  = x + self.tau * x_dot
+        x_dot = damping * x_dot + self.tau * xacc
+        theta = theta + self.tau * theta_dot
         theta_dot = damping*theta_dot + self.tau * thetaacc
         self.state = np.stack([x, x_dot, theta, theta_dot] + sysid_states, axis=1)
 
+        theta_wrap = (theta + np.pi) % (2 * np.pi) - np.pi
+
         if self.fixedlen:
-            loss = np.abs(x) + np.abs(theta) + 0.1*np.abs(x_dot) + 0.1*np.abs(theta_dot)
-            reward = -0.1 * loss
+            #print("x = {}, xdot = {}, th = {}, thdot = {}".format(
+                #x[0], x_dot[0], theta[0], theta_dot[0]))
+            vel_rew = - 0.05 * np.abs(theta_dot) - 0.02 * np.abs(x_dot)
+            reward = -(1.0/np.pi) * np.abs(theta_wrap) - 0.05*np.abs(x) + vel_rew
+            reward[np.abs(x) > self.x_threshold] -= 100
 
             self.tick += 1
-            self.done = (0 * reward).astype(bool)
+            self.done = x != x
             if self.tick >= self.ep_len:
-                self.done = np.logical_not(self.done)
+                self.done = x == x
                 self.reset_done()
                 self.tick = 0
-                self.ep_len *= 1.01
         else:
             self.done = np.logical_or(
                 np.abs(x) > self.x_threshold,
-                np.abs(theta) > self.theta_threshold_radians)
+                np.abs(theta) > 8 * np.pi
+            )
+            theta_wrap = (theta + np.pi) % (2 * np.pi) - np.pi
+            assert all(np.abs(theta_wrap) <= np.pi)
 
-            reward = 1.0 - 0.1*np.abs(x) - 0.1*np.abs(theta) - 0.01*np.abs(x_dot) - 0.01*np.abs(theta_dot
-            reward[self.done] = -1000
-            reward = 0.1 * reward
+            #reward = 0.1 - 0.1*np.abs(x) - 0.1*np.abs(theta_wrap) - 0.01*np.abs(x_dot) - 0.01*np.abs(theta_dot)
+            #reward = 0.1 * reward
+            #reward = 1.0 * (np.abs(theta) < 0.1) + 0.1 * (np.abs(theta < np.pi / 2))
+            reward = -0.1 * np.abs(theta_wrap)
+            reward[self.done] = -100
             self.reset_done()
+
+        self.reward = reward
 
         # after self.reset_done(), all states that were done
         # have already been reset to new states
@@ -156,10 +167,11 @@ class CartPoleEnvSysIDBatch(gym.Env):
 
     def _sample_reset(self):
         x = self.x_threshold / 115.0
-        t = self.theta_threshold_radians / 110.0
+        #t = self.theta_threshold_radians / 110.0
+        th = np.pi
 
         state = np.concatenate([
-            self.np_random.uniform(low=[-x, -x, -t, -t], high=[x, x, t, t], size=(self.N, 4)),
+            self.np_random.uniform(low=[-x, -x, th, 0], high=[x, x, th, 0], size=(self.N, 4)),
             self.sysid_values()], 1)
         assert state.shape == (self.N, self.sysid_dim + 4)
         return state
@@ -211,8 +223,14 @@ class CartPoleEnvSysIDBatch(gym.Env):
             self.track = rendering.Line((0,carty), (screen_width,carty))
             self.track.set_color(0,0,0)
             self.viewer.add_geom(self.track)
+            self.pole = pole
 
         if self.state is None: return None
+
+        color_good = np.array([0.1, 0.8, 0.2])
+        color_bad = np.array([0.9, 0.1, 0.15])
+        r = -max(-1.0, self.reward[0])
+        self.pole.set_color(*(r * color_bad + (1 - r) * color_good))
 
         x = self.state[0]
         cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
